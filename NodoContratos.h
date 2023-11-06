6f7710b075331f43d4f7065e2266b34797195f23
Cristi Lanuza <141888629+C2Lg223@users.noreply.github.com>
2023-11-06 09:06:28 -0800

    Nodo

---
+++ NodoContratos.h
@@ -0,0 +1,20 @@
+#include"Contrato.h"
+
+class NodoContrato {
+
+
+private:
+	Contrato* contratoPtr;
+	NodoContrato* ptrNodo;
+
+
+public:
+	NodoContrato();
+	NodoContrato(Contrato&, NodoContrato*);
+	virtual ~NodoContrato();
+	void setContrato(Contrato&);
+	void setSigNodo(NodoContrato*);
+	Contrato* getContrato();
+	NodoContrato* getSigNodo();
+
+};
