5281d20d840bf2031d3b59a8ad97d8e4095c14d7
Cristi Lanuza <141888629+C2Lg223@users.noreply.github.com>
2023-11-06 23:08:56 -0800

    Interfaz Menu

--- Interfaz.h
+++ Interfaz.h
@@ -5,11 +5,28 @@ using namespace std;
 
 class Interfaz {
 public:
+	//Menus 
+
 	static int menuPrincipal();
+	//Menu Ingresos 
 	static int menuIngresos();
 	static int menuIngresoEmpleados();
 	static int menuIngresoAeronaves();
+	//Segunda opcion del menu principal MANTENIMIENTO
+	static int menuMantenimiento();
+	static int menuMantEmpleado();
+	static int menuMantAeronaves();
+	static int menuMantContratos();
+	//Tercera opcion del menu principla REPORTES 
+	static int menuReportes();
 
+
+
+
+	//Cuarta opcion del menu principla CONSULTA 
+	static int menuConsulta();
+
+	static int DatosEmpleado();
 	static int ingresarPiloto(Aeropuerto* per);
 	static int ingresaCopilotos(Aeropuerto* per);
 	static int ingresaAzafata(Aeropuerto* per);
