a83106ca6ccdd003e7e7019d518323302b7904eb
JUAN GUTIERREZ TREJOS <juan.gutierrez.trejos@est.una.ac.cr>
2023-11-06 21:55:36 -0600

    interfaz

--- Interfaz.h
+++ Interfaz.h
@@ -1,2 +1,27 @@
 #pragma once
-//prueba
+#include<iostream>
+#include"Aeropuerto.h"
+using namespace std;
+
+class Interfaz {
+public:
+	static int menuPrincipal();
+	static int menuIngresos();
+	static int menuIngresoEmpleados();
+	static int menuIngresoAeronaves();
+
+	static int ingresarPiloto(Aeropuerto* per);
+	static int ingresaCopilotos(Aeropuerto* per);
+	static int ingresaAzafata(Aeropuerto* per);
+	static int ingreasaAdministrativos(Aeropuerto* per);
+	static int ingresaMiscelaneo(Aeropuerto* per);
+
+};
+
+
+
+
+
+
+
+
