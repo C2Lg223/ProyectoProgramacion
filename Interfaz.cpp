e35b91134f88f6362bd47f4a432410090fc06ca6
Cristi Lanuza <141888629+C2Lg223@users.noreply.github.com>
2023-11-09 16:08:39 -0800

    Cambios de orden

--- Interfaz.cpp
+++ Interfaz.cpp
@@ -42,102 +42,7 @@ int Interfaz::menuIngresoEmpleados()
 	cin >> opc;
 	return opc;
 }
-int Interfaz::menuMantenimiento() {
-	int opc;
-	system("cls");
-	cout << "***********************  MANTENIMIENTO ******************************" << endl;
-	cout << " 1) Empleado " << endl;
-	cout << " 2) Aeronaves " << endl;
-	cout << " 3) Contratos " << endl;
-	cout << " 4) Atras " << endl;
-	cout << "Digitar una opcion: " << endl;
-	cin >> opc;
-	return opc;
-}
 
- int Interfaz::menuMantEmpleado() {
-	int opc;
-	system("cls");
-	cout << "2.Mantenimiento                               2.1 Empleados " << endl << endl;
-	cout << " 1) Eliminar Empleado " << endl;
-	cout << " 2) Modificar Datos " << endl;
-	cout << "Digitar una opcion: " << endl;
-	cin >> opc;
-	return opc;
-
-}
-
-int Interfaz::menuMantAeronaves() {
-	int opc;
-	system("cls");
-	cout << "2.Mantenimiento                               2.2 Aeronaves " << endl << endl;
-	cout << " 1) Eliminar Aeronave " << endl;
-	cout << " 2) Modificar Datos " << endl;
-	cout << "Digitar una opcion: " << endl;
-	cin >> opc;
-	return opc;
-
-}
-
-int Interfaz::menuMantContratos() {
-	int opc;
-	system("cls");
-	cout << "2.Mantenimiento                               2.3 Contratos  " << endl << endl;
-	cout << " 1) Eliminar Contrato " << endl;
-	cout << " 2) Modificar Datos " << endl;
-	cout << "Digitar una opcion: " << endl;
-	cin >> opc;
-	return opc;
-
-}
-
-
-
-int Interfaz::menuReportes() {
-	int opc;
-
-	system("cls");
-	cout << " <=><=><=><=><=><=><=><=><=><=><=><=><=><=><=><=><=><=><=><=><=>" << endl;
-	cout << "                     3. REPORTES                                " << endl;
-	cout << "  1.  Contratos con sus empleados " << endl;
-	cout << "  2.  Aeronaves con su tripulación.  " << endl;
-	cout << "  3.  Todas las Aeronaves " << endl;
-	cout << "  4.  Pilotos de Aviones de Carga " << endl;
-	cout << "  5.  Aviones con mas de 20 annios" << endl;
-	cout << "  6.  Contratos de Servicios Profesionales " << endl;
-	cout << "  7.  Contratos de Plazo Fijo " << endl;
-	cout << "  8.  Contratos de Tiempo Indefinido" << endl;
-	cout << "  9.  Tripulaciones de Aviones Comerciales " << endl;
-	cout << " 10.  Contratos de Plazo Fijo excedidos " << endl;
-	cout << " 11.  Avion de Carga con mayor area de acceso " << endl;
-	cout << " 12.  Menu Anterior " << endl;
-	cout << endl << endl;
-	cout << " <=><=><=><=><=><=><=><=><=><=><=><=><=><=><=><=><=><=><=><=><=>" << endl;
-
-	cout << "Digitar una opcion: " << endl;
-	cin >> opc;
-	return opc;
-
-}
-
-//MENU CONSULTAS 
-int Interfaz::menuConsulta() {
-	int opc;
-
-	system("cls");
-	cout << " ========================================================================" << endl;
-	cout << "                      CONSULTA " << endl << endl;
-	cout << "                 1. Empleado " << endl;
-	cout << "                 2. Contrato " << endl;
-	cout << "                 3. Aeronave " << endl;
-	cout << "                 4. Atras " << endl;
-	cout << " ========================================================================" << endl;
-
-	cout << "Digitar una opcion: " << endl;
-	cin >> opc;
-	return opc;
-
-}
 
 void Interfaz::ingresarPiloto(Aeropuerto* aero){
 	Empleado* p = NULL;
@@ -257,6 +162,104 @@ void Interfaz::ingresaMiscelaneo(Aeropuerto* aero){
 	cout << "---------------------------------------------" << endl;
 
 	Mis = new Miscelaneos(escolaridad, labor, ced, nom, ocu, ed);
+}
+
+
+int Interfaz::menuMantenimiento() {
+	int opc;
+	system("cls");
+	cout << "***********************  MANTENIMIENTO ******************************" << endl;
+	cout << " 1) Empleado " << endl;
+	cout << " 2) Aeronaves " << endl;
+	cout << " 3) Contratos " << endl;
+	cout << " 4) Atras " << endl;
+	cout << "Digitar una opcion: " << endl;
+	cin >> opc;
+	return opc;
+}
+
+int Interfaz::menuMantEmpleado() {
+	int opc;
+	system("cls");
+	cout << "2.Mantenimiento                               2.1 Empleados " << endl << endl;
+	cout << " 1) Eliminar Empleado " << endl;
+	cout << " 2) Modificar Datos " << endl;
+	cout << "Digitar una opcion: " << endl;
+	cin >> opc;
+	return opc;
+
+}
+
+int Interfaz::menuMantAeronaves() {
+	int opc;
+	system("cls");
+	cout << "2.Mantenimiento                               2.2 Aeronaves " << endl << endl;
+	cout << " 1) Eliminar Aeronave " << endl;
+	cout << " 2) Modificar Datos " << endl;
+	cout << "Digitar una opcion: " << endl;
+	cin >> opc;
+	return opc;
+
+}
+
+int Interfaz::menuMantContratos() {
+	int opc;
+	system("cls");
+	cout << "2.Mantenimiento                               2.3 Contratos  " << endl << endl;
+	cout << " 1) Eliminar Contrato " << endl;
+	cout << " 2) Modificar Datos " << endl;
+	cout << "Digitar una opcion: " << endl;
+	cin >> opc;
+	return opc;
+
+}
+
+
+
+int Interfaz::menuReportes() {
+	int opc;
+
+	system("cls");
+	cout << " <=><=><=><=><=><=><=><=><=><=><=><=><=><=><=><=><=><=><=><=><=>" << endl;
+	cout << "                     3. REPORTES                                " << endl;
+	cout << "  1.  Contratos con sus empleados " << endl;
+	cout << "  2.  Aeronaves con su tripulación.  " << endl;
+	cout << "  3.  Todas las Aeronaves " << endl;
+	cout << "  4.  Pilotos de Aviones de Carga " << endl;
+	cout << "  5.  Aviones con mas de 20 annios" << endl;
+	cout << "  6.  Contratos de Servicios Profesionales " << endl;
+	cout << "  7.  Contratos de Plazo Fijo " << endl;
+	cout << "  8.  Contratos de Tiempo Indefinido" << endl;
+	cout << "  9.  Tripulaciones de Aviones Comerciales " << endl;
+	cout << " 10.  Contratos de Plazo Fijo excedidos " << endl;
+	cout << " 11.  Avion de Carga con mayor area de acceso " << endl;
+	cout << " 12.  Menu Anterior " << endl;
+	cout << endl << endl;
+	cout << " <=><=><=><=><=><=><=><=><=><=><=><=><=><=><=><=><=><=><=><=><=>" << endl;
+
+	cout << "Digitar una opcion: " << endl;
+	cin >> opc;
+	return opc;
+
+}
+
+//MENU CONSULTAS 
+int Interfaz::menuConsulta() {
+	int opc;
+
+	system("cls");
+	cout << " ========================================================================" << endl;
+	cout << "                      CONSULTA " << endl << endl;
+	cout << "                 1. Empleado " << endl;
+	cout << "                 2. Contrato " << endl;
+	cout << "                 3. Aeronave " << endl;
+	cout << "                 4. Atras " << endl;
+	cout << " ========================================================================" << endl;
+
+	cout << "Digitar una opcion: " << endl;
+	cin >> opc;
+	return opc;
+
 }
 
 int Interfaz::menuModificarEmple() {
